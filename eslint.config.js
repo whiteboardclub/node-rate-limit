9ccba8ed057ec2c56b3a754285fee12b7787db49
sameerahmed56 <samahmed78656@gmail.com>
2025-01-05 02:40:12 +0530

    Update test folder name to tests Fix eslint config for jest Add EOL

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import jestPlugin from "eslint-plugin-jest";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      jest: jestPlugin, // Specify the Jest plugin as an object
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
+    ignores: [".gitignore"],
+  },
 ];
